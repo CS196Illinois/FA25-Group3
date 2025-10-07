13398073877a51a37e646d33c60b33fef6db3a6d
Joseph <josephng1246@gmail.com>
2025-10-07 17:39:39 -0500

    saved all changes to the home page

--- Frontend/src/app/page.js
+++ Frontend/src/app/page.js
@@ -1,7 +1,10 @@
 // Home Page
 // Route: /
-import React, { use_state } from "react";
+"use client"
+
+import React from "react";
 import styles from "./page.module.css";
+import Button from "../components/Button";
 
 function startButton() {
   const handleClick = () => {
@@ -24,10 +27,93 @@ export default function Home() {
           <div className={styles['explore-uiuc-text']}>EXPLORE URBANA-CHAMPAIGN!</div>
         </div>
 
+        {/* Original Start Button */}
         <button className={styles['start-button']}> 
-        {/* need to add onClick function here */}
           <div className={styles['button-text']}>Start!</div>
         </button>
+
+        {/* 10 Component Buttons
+        <div className={styles['button-grid']}>
+          <Button 
+            variant="primary" 
+            size="medium" 
+            onClick={() => console.log('Primary clicked')}
+          >
+            Primary
+          </Button>
+
+          <Button 
+            variant="secondary" 
+            size="medium" 
+            onClick={() => console.log('Secondary clicked')}
+          >
+            Secondary
+          </Button>
+
+          <Button 
+            variant="success" 
+            size="medium" 
+            onClick={() => console.log('Success clicked')}
+          >
+            Success
+          </Button>
+
+          <Button 
+            variant="danger" 
+            size="medium" 
+            onClick={() => console.log('Danger clicked')}
+          >
+            Danger
+          </Button>
+
+          <Button 
+            variant="warning" 
+            size="medium" 
+            onClick={() => console.log('Warning clicked')}
+          >
+            Warning
+          </Button>
+
+          <Button 
+            variant="info" 
+            size="medium" 
+            onClick={() => console.log('Info clicked')}
+          >
+            Info
+          </Button>
+
+          <Button 
+            variant="light" 
+            size="medium" 
+            onClick={() => console.log('Light clicked')}
+          >
+            Light
+          </Button>
+
+          <Button 
+            variant="dark" 
+            size="medium" 
+            onClick={() => console.log('Dark clicked')}
+          >
+            Dark
+          </Button>
+
+          <Button 
+            variant="purple" 
+            size="medium" 
+            onClick={() => console.log('Purple clicked')}
+          >
+            Purple
+          </Button>
+
+          <Button 
+            variant="teal" 
+            size="medium" 
+            onClick={() => console.log('Teal clicked')}
+          >
+            Teal
+          </Button> */}
+        {/* </div> */}
       </div>
     </div>
   );
