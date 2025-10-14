0aa1836d39e5f2f2d83aa3b0875fd3da3e08a0f0
Neila <neiladaigle@gmail.com>
2025-10-14 16:17:40 -0500

    Add working sliders and update SettingsModal styling

--- Frontend/src/components/SettingsModal.js
+++ Frontend/src/components/SettingsModal.js
@@ -7,7 +7,8 @@ export default function SettingsModal() {
   // useState allows the variable to be changed (i.e. allows the user to close/open menu)
   // use "open" to read the state and "setOpen" to set the state, false (closed) by default
   const [open, setOpen] = useState(false);
-
+  const [effectVolume, setEffectVolume] = useState(50);
+  const [musicVolume, setMusicVolume] = useState(50);
   // change this stuff below, this is just so we have a way to actually open the settings for now
   return (
     <>
@@ -17,7 +18,29 @@ export default function SettingsModal() {
           <div className={styles.modalContent}>
             <h2>Settings</h2>
             <button onClick={() => setOpen(false)}>Close</button>
-            {/* Settings content here */}
+            {/* Effect Volume Slider */}
+            <div className={styles.sliderContainer}>
+              <label>Effect Volume: {effectVolume}%</label>
+              <input 
+                type="range"
+                min="0"
+                max="100"
+                value={effectVolume}
+                onChange={(e) => setEffectVolume(e.target.value)}
+              />
+              </div>
+            {/* Music Volume Slider */}
+            <div className={styles.sliderContainer}>
+              <label>Music Volume: {musicVolume}%</label>
+              <input 
+                type="range"
+                min="0"
+                max="100"
+                value={musicVolume}
+                onChange={(e) => setMusicVolume(e.target.value)}
+              />
+            </div>
+
           </div>
         </div>
       )}
