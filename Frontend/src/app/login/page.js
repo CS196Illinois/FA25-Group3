2bf42341ba334aed78ff4190bb66e4109c50f873
elizabethmanuel <elizabethmanuel@wirelessprv-10-193-163-191.near.illinois.edu>
2025-10-18 12:42:03 -0500

    login button fixed

--- Frontend/src/app/login/page.js
+++ Frontend/src/app/login/page.js
@@ -1,24 +1,27 @@
+"use client";
+
 import styles from "./page.module.css";
 import LoginButton from "../../components/LoginButton.js";
-// import { signInWithGoogle } from "./firebase-config";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
-function Login() {
-  
+export default function Login() {
+  const router = useRouter();
+
+  const handleLoginSuccess = () => {
+    router.push("/game"); // redirect to /game after login
+  };
+
   return (
-    <Link href="/">
-      <div className={styles.App}>
-        <header className={styles["App-header"]}>
-          <p className={styles["App-para"]}>
-            Welcome to
-          </p>
-          <img src="/geouiuc_logo.png" className={styles["App-logo"]} alt="logo" />
-          <p>
-          </p>
-          <LoginButton />
-        </header>
-      </div>
-    </Link>
+    <div className={styles.App}>
+      <header className={styles["App-header"]}>
+        <p className={styles["App-para"]}>Welcome to</p>
+        <img
+          src="/geouiuc_logo.png"
+          className={styles["App-logo"]}
+          alt="logo"
+        />
+        <LoginButton onLoginSuccess={handleLoginSuccess} />
+      </header>
+    </div>
   );
 }
-export default Login;
