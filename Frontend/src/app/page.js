118f359db04631e508421b2b50d95aca51091052
Joseph <josephng1246@gmail.com>
2025-10-18 10:43:24 -0500

    Added elements to the homepage, including logo and start button

--- Frontend/src/app/page.js
+++ Frontend/src/app/page.js
@@ -1,20 +1,12 @@
 // Home Page
 // Route: /
-"use client"
-
-import React from "react";
+import React, { use_state } from "react";
 import styles from "./page.module.css";
-import Button from "../components/Button";
-import Link from "next/link";
 
-function StartButton() {
-  return (
-    <Link href="/login">
-      <button className={styles['start-button']}>
-        <div className={styles['button-text']}>Start!</div>
-      </button>
-    </Link>
-  );
+function startButton() {
+  const handleClick = () => {
+    // window.location.href = "/new-page"; // goes to game page to begin or login page
+  }
 }
 
 
@@ -32,92 +24,10 @@ export default function Home() {
           <div className={styles['explore-uiuc-text']}>EXPLORE URBANA-CHAMPAIGN!</div>
         </div>
 
-        {/* Original Start Button */}
-        <div className={styles['logo-container']}>
-          <StartButton />
-        </div>
-        {/* 10 Component Buttons
-        <div className={styles['button-grid']}>
-          <Button 
-            variant="primary" 
-            size="medium" 
-            onClick={() => console.log('Primary clicked')}
-          >
-            Primary
-          </Button>
-
-          <Button 
-            variant="secondary" 
-            size="medium" 
-            onClick={() => console.log('Secondary clicked')}
-          >
-            Secondary
-          </Button>
-
-          <Button 
-            variant="success" 
-            size="medium" 
-            onClick={() => console.log('Success clicked')}
-          >
-            Success
-          </Button>
-
-          <Button 
-            variant="danger" 
-            size="medium" 
-            onClick={() => console.log('Danger clicked')}
-          >
-            Danger
-          </Button>
-
-          <Button 
-            variant="warning" 
-            size="medium" 
-            onClick={() => console.log('Warning clicked')}
-          >
-            Warning
-          </Button>
-
-          <Button 
-            variant="info" 
-            size="medium" 
-            onClick={() => console.log('Info clicked')}
-          >
-            Info
-          </Button>
-
-          <Button 
-            variant="light" 
-            size="medium" 
-            onClick={() => console.log('Light clicked')}
-          >
-            Light
-          </Button>
-
-          <Button 
-            variant="dark" 
-            size="medium" 
-            onClick={() => console.log('Dark clicked')}
-          >
-            Dark
-          </Button>
-
-          <Button 
-            variant="purple" 
-            size="medium" 
-            onClick={() => console.log('Purple clicked')}
-          >
-            Purple
-          </Button>
-
-          <Button 
-            variant="teal" 
-            size="medium" 
-            onClick={() => console.log('Teal clicked')}
-          >
-            Teal
-          </Button> */}
-        {/* </div> */}
+        <button className={styles['start-button']}> 
+        {/* need to add onClick function here */}
+          <div className={styles['button-text']}>Start!</div>
+        </button>
       </div>
     </div>
   );
