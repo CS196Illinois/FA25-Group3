b7781ccb5765f3d40ab97be1e4747ebaacedb2e2
SkiesShaper <sporangeisaword@gmail.com>
2025-10-17 20:08:42 -0500

    Current state of the gameplay page

---
+++ Frontend/src/app/game/project-scripts/test.js
@@ -0,0 +1,21 @@
+async function initialize2() {
+    const location = { lat: getRandomPoint().lat, long: getRandomPoint().long }
+    const ControlPosition = await google.maps.importLibrary("core")
+    panorama = new google.maps.StreetViewPanorama(
+        pano.current,
+        {
+            position: location,
+            pov: {
+                heading: 0,
+                pitch: 0,
+            },
+            addressControl: false,
+            fullscreenControl: false,
+            showRoadLabels: false,
+            zoomControl: false,
+            panControl: false,
+            linksControl: false
+        },
+    );
+    startRound()
+}
