6b13efcfffcbded62d550827dc6ff0ee98682a10
Emily Zheng <emilyzheng3250@gmail.com>
2025-10-27 17:10:09 -0500

    <added buttons and logo to profile page>

--- Frontend/src/app/profile/page.js
+++ Frontend/src/app/profile/page.js
@@ -2,23 +2,32 @@
 // Route: /profile
 
 "use client"
-
 import styles from "./page.module.css";
-import signInWithGoogle  from "@/components/firebase-config";
-
+import React, { useEffect, useState } from 'react';
+import SignOut from "@/components/SignoutButton";
+import DeleteAccount from "@/components/DeleteAccountButton";
+import signInWithGoogle from "@/components/firebase-config";
+import Link from "next/link";
 
 export default function Profile() {
   return (
     <div className={styles.container}>
 
       <div className={styles.profileContainer}>
-        <img src={localStorage.getItem("profilePic")} />
-        <br />
-        <br />
+        <div>
+          <Link href= "/">
+            <img src="./logo.png" style={{ maxHeight: "60px" }} />
+          </Link>
+        </div>
+        <img className={styles.img} src={localStorage.getItem("profilePic")} />
         <br />
         <h1>{localStorage.getItem("name")}</h1>
         <br />
         <h3>Email: {localStorage.getItem("email")}</h3>
+        <br />
+        <SignOut />
+        <br />
+        <DeleteAccount />
 
       </div>
 
