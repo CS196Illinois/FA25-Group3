d99e7c402964e80b49a6abcce28f16abddaf0862
Emily Zheng <emilyzheng3250@gmail.com>
2025-10-27 15:20:22 -0500

    <skeleton bare bones of profile page>

--- Frontend/src/app/profile/page.js
+++ Frontend/src/app/profile/page.js
@@ -4,8 +4,19 @@ import styles from "./page.module.css";
 
 export default function Profile() {
   return (
-    <div style={styles}>
-	
+    <div className={styles.container}>
+
+      <div className={styles.profileContainer}>
+        <h1>[Username]</h1>
+        <h3>Email: {}</h3>
+      </div>
+
+      <div className={styles.profileContainer2}>
+        <h1>High Score: {}</h1>
+        <h1>Total Points: {}</h1>
+        <h1>Daily Streak: {}</h1>
+      </div>
+      
     </div>
   );
 }
