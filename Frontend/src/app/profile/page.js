87280fd4aadbf7b2f6f458f6036a3c13379c9db8
Emily Zheng <emilyzheng3250@gmail.com>
2025-10-27 16:19:02 -0500

    <profile image, name, and email added to profile>

--- Frontend/src/app/profile/page.js
+++ Frontend/src/app/profile/page.js
@@ -1,19 +1,34 @@
 // Profile Page
 // Route: /profile
+
+"use client"
+
 import styles from "./page.module.css";
+import signInWithGoogle  from "@/components/firebase-config";
+
 
 export default function Profile() {
   return (
     <div className={styles.container}>
 
       <div className={styles.profileContainer}>
-        <h1>[Username]</h1>
-        <h3>Email: {}</h3>
+        <img src={localStorage.getItem("profilePic")} />
+        <br />
+        <br />
+        <br />
+        <h1>{localStorage.getItem("name")}</h1>
+        <br />
+        <h3>Email: {localStorage.getItem("email")}</h3>
+
       </div>
 
       <div className={styles.profileContainer2}>
         <h1>High Score: {}</h1>
+        <br />
+        <br />
         <h1>Total Points: {}</h1>
+        <br />
+        <br />
         <h1>Daily Streak: {}</h1>
       </div>
       
