46bbc0ac400c28351410fbc6164a4801340d2a94
Emily Zheng <emilyzheng3250@gmail.com>
2025-10-28 17:23:46 -0500

    <fixed profile information and issue added a button that doesn't work>

--- Frontend/src/app/profile/page.js
+++ Frontend/src/app/profile/page.js
@@ -2,42 +2,44 @@
 // Route: /profile
 
 "use client"
+
 import styles from "./page.module.css";
-import React, { useEffect, useState } from 'react';
+import Link from "next/link";
+import React, { useState, useEffect } from 'react';
 import SignOut from "@/components/SignoutButton";
 import DeleteAccount from "@/components/DeleteAccountButton";
-import signInWithGoogle from "@/components/firebase-config";
-import Link from "next/link";
 
 export default function Profile() {
+  const [profilePic, setProfilePic] = useState();
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+
+  useEffect(() => {
+    setProfilePic(localStorage.getItem("profilePic"));
+    setName(localStorage.getItem("name"));
+    setEmail(localStorage.getItem("email"));
+  }, []);
+
   return (
     <div className={styles.container}>
-
+      <div>
+        <Link href= "/">
+          <img src="./logo.png" style={{ maxHeight: "60px" }} />
+        </Link>
+      </div>
+      
       <div className={styles.profileContainer}>
-        <div>
-          <Link href= "/">
-            <img src="./logo.png" style={{ maxHeight: "60px" }} />
-          </Link>
-        </div>
-        <img className={styles.img} src={localStorage.getItem("profilePic")} />
-        <br />
-        <h1>{localStorage.getItem("name")}</h1>
-        <br />
-        <h3>Email: {localStorage.getItem("email")}</h3>
-        <br />
+        <img className={styles.img} src={profilePic} />
+        <h1>{name}</h1>
+        <h3>Email: {email}</h3>
         <SignOut />
         <br />
         <DeleteAccount />
-
       </div>
 
       <div className={styles.profileContainer2}>
         <h1>High Score: {}</h1>
-        <br />
-        <br />
         <h1>Total Points: {}</h1>
-        <br />
-        <br />
         <h1>Daily Streak: {}</h1>
       </div>
       
