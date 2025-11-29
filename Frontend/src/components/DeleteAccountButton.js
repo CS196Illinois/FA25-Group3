1832792ca377893951757414882abfecccd1c637
kaixhu <kaitlynhu01@gmail.com>
2025-11-28 18:35:16 -0600

    Created a Confirm Delete popup that appears when users click on delete account, also implemented a delete account function that deletes the user from the authentication log in Firebase (still need to figure out how to delete user from actual firestore database though

--- Frontend/src/components/DeleteAccountButton.js
+++ Frontend/src/components/DeleteAccountButton.js
@@ -1,7 +1,35 @@
+
+import { auth } from "../components/firebase-config";
+import {
+  GoogleAuthProvider,
+  deleteUser,
+  reauthenticateWithPopup
+} from "firebase/auth";
+import { useRouter} from "next/navigation"; 
+
 import styles from "./DeleteAccount.module.css"
+import Popup from "./ConfirmDeletePopUp";
+import { useState } from 'react';
 
 export default function DeleteAccount() {
+    const router = useRouter();
+    const handleDelete = async () => {
+        const user = auth?.currentUser;
+        try {
+            const googleProvider = new GoogleAuthProvider();
+            await reauthenticateWithPopup(user, googleProvider);
+            await deleteUser(user);
+            router.push("/login")
+        }
+        catch (error) {}
+    }
+   const [buttonPopup, setButtonPopup] = useState(false);
     return (
-        <button className={styles.delete}>Delete Account</button>
+        <>
+            <button className={styles.delete} onClick={() => setButtonPopup(true)}>Delete Account</button>
+            <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+                <button className={styles.delete} onClick={handleDelete}>Confirm Delete</button>
+            </Popup>
+        </>
     );
 }
